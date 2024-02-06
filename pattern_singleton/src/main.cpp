388c78e5576b714be03e6df43a535b6deffe77fa
Erik Schumacher <erik.d.schumacher@gmail.com>
2024-02-05 16:51:42 -0800

    Singleton Pattern: implemented basic example

--- pattern_singleton/src/main.cpp
+++ pattern_singleton/src/main.cpp
@@ -1,8 +1,18 @@
 
 #include <iostream>
+#include "singleton.hpp"
 
 int main() {
   std::cout << "Singleton Pattern Example:\n\n";
+
+  Singleton::get_instance().print_address();
+
+  Singleton &singleton_ref = Singleton::get_instance();
+  Singleton *singleton_ptr = &Singleton::get_instance();
+
+  singleton_ref.print_address();
+  singleton_ptr->print_address();
+  Singleton::get_instance().print_address();
 
   return 0;
 }
